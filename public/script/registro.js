29d82dbf4a5c56b2be3fb02f469a0ee8e3f0431c
Matheus <matheusvalle06@gmail.com>
2026-01-15 16:32:17 -0300

    finalização validações de login e registro

---
+++ public/script/registro.js
@@ -0,0 +1,42 @@
+function login() {
+    var email = input_email.value
+    var senha = input_senha.value
+
+    if (email == "" || email == undefined || senha == "" || senha == undefined) {
+        console.log("input sem nada!!!!")
+        div_erro.innerHTML = `<b style="color: red;">Inputs Vazios!</b>`
+    } else {
+        console.log("passou")
+        div_erro.innerHTML = `` 
+    }
+    
+}
+
+function registro() {
+    var nome = input_nome.value 
+    var email = input_email.value 
+    var senha = input_senha.value 
+    var conf_senha = input_confirmar_senha.value
+
+    if (nome == "" || email == "" || senha == "" || conf_senha == "") {
+        div_erro.innerHTML = `<b style="color: red;">Inputs Vazios!</b><br>` 
+    }
+
+    if (nome.length > 45) {
+         div_erro.innerHTML += `<b style="color: red;">Nome muito longo!</b><br>` 
+    }
+
+    if (email.includes('@') == false || email.includes('.com') == false) {
+         div_erro.innerHTML += `<b style="color: red;">Email incorreto!</b><br>`
+    }
+
+    if (senha.length < 8) {
+         div_erro.innerHTML += `<b style="color: red;">Senha menor que 8 caracteres!</b><br>`
+    }
+
+    if (senha != conf_senha) {
+         div_erro.innerHTML += `<b style="color: red;">Senhas não sao iguais!</b><br>`
+    }
+
+   
+}
